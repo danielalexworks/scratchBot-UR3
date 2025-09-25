003119e7d709f51dd0c4c9e6a60db7719d397fb9
danielalexworks <danielalexworks@gmail.com>
2025-09-25 13:18:14 -0400

    reorg

--- public/scripts/pythonCalls.js
+++ public/scripts/pythonCalls.js
@@ -1,16 +1,49 @@
 host = "192.168.1.59"
+pythonhost = "192.168.1.9"
+pythonhost ="127.0.0.1"
+pythonhost = (window.location.host).split(':')[0];
+console.log(pythonhost);
+var wsRunning = false;
+var socket;
+ function startWS(){
+    socket = new WebSocket('ws://'+ pythonhost + ':8765');
+
+    socket.onmessage = function(event) {
+        data = JSON.parse(event.data);
+        //console.log("DATA LINES: ");
+        //console.log(data['lines'])
+        //console.log('Message from server:', event.data);
+        scratched = data['scratched'];
+        //console.log(scratched);
+        if(scratched != 'ready'){
+           // console.log('scratched update')
+            updatedTicket(scratched);
+        }
+
+        drawLines(data['lines']);
+
+        // You can trigger DOM changes or other actions here
+    };
+    socket.onopen = function() {
+        //console.log('WebSocket connection established');
+        wsRunning = true;
+
+    };
+ }
+
 
 function getSettingsData(){
     fetch('/api/get_settings_data', {
-        method: 'POST',
+        method: 'GET',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ data: 'all' }),
+        //body: JSON.stringify({ data: 'all' }),
     })
     .then(response => response.json())
     .then(data => {
         fillSettings(data);
+        getTicket();
     })
     .catch((error) => {
         console.error('Error:', error);
@@ -45,7 +78,6 @@ function updateSettings(data = false){
 }
 
 function manualAction(action, value = false, direction = false){
-   
     data = {}
     data['action'] = action;
     if( value ) data['value'] = value;
@@ -53,6 +85,53 @@ function manualAction(action, value = false, direction = false){
     console.log(data);
 
     fetch('/api/manual_actions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function adjustTCP(action, value = false, direction = false){
+    data = {}
+    data['action'] = action;
+    if( value ) data['value'] = value;
+    if( direction ) data['direction'] = direction;
+    console.log(data);
+
+    fetch('/api/adjust_TCP', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+
+function manualRotate(action, value = false, direction = false){
+    data = {}
+    data['action'] = action;
+    if( value ) data['value'] = value;
+    if( direction ) data['direction'] = direction;
+    console.log(data);
+
+    fetch('/api/manual_rotate', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
@@ -83,7 +162,56 @@ function programAction(action){
     .then(response => response.json())
     .then(data => {
         console.log(data)
-        getSettingsData()
+
+        if(action != 'nextgroup')
+            getSettingsData()
+
+        if( action == 'start' && settings['status']['value'] != 'paused'){
+            startWS();
+        }
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function alignTicket(pl){
+   
+    data = {}
+    data['location'] = pl;
+
+    fetch('/api/align_ticket', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        console.log("READY TO ALIGN TICKET")
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function enableFreeDrive(){
+   
+    fetch('/api/enable_free_drive', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+
+        
     })
     .catch((error) => {
         console.error('Error:', error);
@@ -91,7 +219,7 @@ function programAction(action){
 }
 
 function getTicket(){
-   
+    
     console.log('getticket');
     fetch('/api/get_ticket', {
         method: 'GET',
@@ -102,7 +230,7 @@ function getTicket(){
     })
     .then(response => response.json())
     .then(data => {
-        
+        console.log(data);
         displayTicket(data)
     })
     .catch((error) => {
@@ -110,8 +238,37 @@ function getTicket(){
     });
 }
 
+function estimateTime(){
+    console.log('et)');
+    $('#overlay').removeClass('hideit');
+    fetch('/api/estimate_time', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+       
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        alert("Program will take an estimated:\r\n" + data['result'] + " minutes +/-20% or so");
+        $('#overlay').addClass('hideit');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+
+//TODO move to front end helpers
 function scratchedStatus(){
    
+
+   if(!wsRunning){
+            startWS();
+        }
+
+        /*
     console.log('scratchStatus');
     fetch('/api/scratch_status', {
         method: 'GET',
@@ -123,7 +280,191 @@ function scratchedStatus(){
     .then(response => response.json())
     .then(data => {
         //getSettingsData()
-        updatedTicket(data)
+        updatedTicket(data['result'])
+        if(!wsRunning){
+            startWS();
+        }
+        //drawLines(data['lines'])
+        
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+    */
+}
+
+function goToPose(pose){
+    data = {}
+    data['pose'] = pose;
+
+    fetch('/api/go_to_pose', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('--------MOVED TO ' + pose)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function setNewStartingPose(action){
+
+    $('#overlay').removeClass('hideit');
+        var counter = 30
+        let inter = setInterval(() =>{
+            counter--;
+            if(counter <0){
+                $('#overlay').addClass('hideit');
+                clearInterval(inter);
+            }else{
+                $('#seconds').html(counter);
+            }
+        },1000)
+
+
+    fetch('/api/set_new_starting_pose', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log("DATA " + data)
+        //start pose timer
+        
+
+
+
+
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+
+function shutdown(){
+    
+    fetch('/api/shutdown', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        }
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('-------- SHUTDOWN');
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function goToBox(box){
+    
+    data = {};
+    data['box'] = box;
+
+    fetch('/api/go_to_box', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('--------MOVED TO ' + box)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function scratchABox(box){
+    data = {};
+    data['box'] = box;
+
+    fetch('/api/scratch_single_box', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('--------SCRATCHED BOX #' + box)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+
+function sendCamTo(loc){
+    console.log("MOVING TO " + loc);
+    data = {};
+    data['loc'] = loc
+    fetch("/api/send_cam_to", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('--------MOVED TO ' + loc)
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+
+function updateArduinoCamSettings(){
+    data = {}
+    fetch("/api/update_arduino_cam_settings", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('cam position found')
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+}
+function figureCamPosition(){
+    data = {}
+    fetch("/api/figure_cam_position", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        console.log('cam position found')
     })
     .catch((error) => {
         console.error('Error:', error);
--- threadManager.py
+++ threadManager.py
@@ -1,43 +1,119 @@
 import threading
 import programs
-import moneyBadger_GUI as mb
+import settings
+import asyncio
+import websockets
+import json
 
 pauseEvent = threading.Event()
+stop_event = threading.Event()
+task_thread = None
+socket_thread = None
+
 
 def isPaused():
-    if(mb.getSett('status') == 'paused') :
+    if(settings.getSett('status') == 'paused') :
         return True
     else:
         return False
 
 def pauseThread():
-    
     pauseEvent.clear()
-    mb.setSett('status','paused')
+    settings.setSett('status','paused')
     print("THREAD PAUSED---")
 
+def stopThread():
+    stop_event.clear()
+    settings.setSett('status','ready')
+    print('PROGRAM / THREAD STOPPED')
+
 def unpauseThread():
-    
     pauseEvent.set()
-    mb.setSett('status', 'in progress')
+    settings.setSett('status', 'in progress')
 
 def checkPause():
     pauseEvent.wait()
+
+def checkStop():
+    return stop_event.is_set()
     
 
 def startProgram(program, scratchArea):
-    
+    global task_thread
+    global socket_thread
+
+    if task_thread and task_thread.is_alive():
+        print("Stopping existing thread...")
+        stop_event.set()  # Signal the thread to stop
+        task_thread.join()  # Wait for the thread to finish
+        if socket_thread and socket_thread.is_alive():
+            print("Stopping existing ws thread...")
+            socket_thread.join()  # Wait for the thread to finish
+
     if(program == 'all'):
         task_thread = threading.Thread(target=programs.scratchAllInOrder, args = (scratchArea,))
-        #programs.scratchAllInOrder(scratchArea)
+        
     elif(program == 'random'):
         task_thread = threading.Thread(target=programs.doAllRandom, args = (scratchArea,))
-        #programs.doAllRandom(scratchArea)
-    elif(program == 'numbers before prizes'):
-        task_thread = threading.Thread(target=programs.scratchNumbersBeforePrizes, args = (scratchArea,))
-        #programs.scratchNumbersBeforePrizes(scratchArea)
+        
+    elif(program == 'scratch by group'):
+        task_thread = threading.Thread(target=programs.scratchByGroup, args = (scratchArea,))
+
+    elif(program == 'scratch by group interruptable'):
+        task_thread = threading.Thread(target=programs.scratchByGroupInterruptable, args = (scratchArea,))
+        
 
     task_thread.daemon = True  # Make the thread exit when the main program exits
     task_thread.start()
-    mb.setSett('status','in progress')
+   
+    startWebSocketThread()
+
+    settings.setSett('status','in progress')
     pauseEvent.set()
+
+
+def togglePause(status = 'none'):
+    if status == 'none':
+        status = settings.getSett('status')
+    if status == 'in progress':
+        settings.setSett('status','paused')
+        pauseThread()
+    elif status == 'paused':
+        settings.setSett('status','in progress')
+        unpauseThread()
+
+
+#+++++++++++++++++++++++++       WEBSOCKET
+#sends lines to GUI if any new ones are created
+oldlines = []
+async def websocket_handler(websocket, path):
+    global oldlines
+    while True:# not checkPause():
+        lines = programs.getLines()
+        scratched = programs.getScratchedInfo() 
+        payload = {'lines': lines,'scratched' :  scratched}
+        if len(lines) > 0:
+            await websocket.send(json.dumps(payload))
+
+        await asyncio.sleep(2)
+        
+
+
+def run_websocket_server():
+    print("|+++++++++++++++++++++++++++++++++++")
+    print("|+++++++++++++++++++++++++++++++++++")
+    print("|+++++++++++++++++++++++++++++++++++")
+    print('..starting ws server')
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    ws_server = websockets.serve(websocket_handler, '0.0.0.0', 8765)
+    loop.run_until_complete(ws_server)
+    loop.run_forever()
+
+
+def startWebSocketThread():
+
+    ws_thread = threading.Thread(target=run_websocket_server)
+    ws_thread.daemon = True
+    ws_thread.start()
+    return ws_thread
